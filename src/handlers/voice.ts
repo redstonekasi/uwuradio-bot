2c51ea42cabce02b2a817bcebac63f56cb3839f1
redstonekasi <kasi@hanneskann.de>
2022-11-27 15:23:48 +0100

    the guh

--- src/handlers/voice.ts
+++ src/handlers/voice.ts
@@ -3,21 +3,18 @@ import { isInChannel, joinChannel, leaveChannel } from "../lib/voice";
 
 export default async function voiceStateHandler() {
   client.on("voiceStateUpdate", async (oldState, newState) => {
-    if (oldState.member?.id === client.user?.id) return;
-
     if (
-      newState.channelId !== oldState.channelId &&
-      newState.channel?.id === client.config.channels[newState.guild.id]
-    ) {
+      oldState.member?.id === client.user?.id ||
+      newState.channelId === oldState.channelId
+    ) return;
+
+    if (newState.channel?.id === client.config.channels[newState.guild.id]) {
       // User joined a channel we care about.
       if (isInChannel(newState.channel)) return;
       try {
         joinChannel(newState.channel);
       } catch {}
-    } else if (
-      newState.channelId !== oldState.channelId &&
-      oldState.channel?.id === client.config.channels[oldState.guild.id]
-    ) {
+    } else if (oldState.channel?.id === client.config.channels[oldState.guild.id]) {
       // User left a channel we care about
       if (oldState.channel.members.size === 1)
         leaveChannel(oldState.channel);
